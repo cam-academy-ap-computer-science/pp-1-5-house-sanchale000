c69927c02933155f7a575eb1337db74754404b77
sanchale000 <sanchale000@CAM-LAB-24.bgsd.k12.wa.us>
2019-09-16 07:46:16 -0700

    Finished so far as I know.

--- src/House.java
+++ src/House.java
@@ -2,8 +2,73 @@
 public class House {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		House1();
+		Malt1();
+		House2();
+		Rat1();
+		Malt2();
+		House2();
+		Cat1();
+		Rat2();
+		Malt2();
+		House2();
+		Dog1();
+		Cat2();
+		Rat2();
+		Malt2();
+		House2();
+		Cow1();
+		Dog2();
+		Cat2();
+		Rat2();
+		Malt2();
+		House2();
+		Maiden1();
+		Cow2();
+		Dog2();
+		Cat2();
+		Rat2();
+		Malt2();
+		House2();
 
 	}
-
+	public static void House1() {
+		System.out.println("This is the house that Jack built.\n");
+	}
+	public static void House2() {
+		System.out.println("That lay in the house that Jack built.\n");
+	}
+	public static void Malt1() {
+		System.out.println("This is the malt");
+	}
+	public static void Malt2() {
+		System.out.println("That ate the malt");
+	}
+	public static void Rat1() {
+		System.out.println("This the rat,");
+	}
+	public static void Rat2() {
+		System.out.println("That killed the rat,");
+	}
+	public static void Cat1() {
+		System.out.println("This is the cat,");
+	}
+	public static void Cat2() {
+		System.out.println("That worried the cat,");
+	}
+	public static void Dog1() {
+		System.out.println("This is the dog,");
+	}
+	public static void Dog2() {
+		System.out.println("That tossed the dog,");
+	}
+	public static void Cow1() {
+		System.out.println("This is the cow with the crumpled horn,");
+	}
+	public static void Cow2() {
+		System.out.println("That milked the cow with the crumpled horn,");
+	}
+	public static void Maiden1() {
+		System.out.println("This is the maiden all forlorn,");
+	}
 }
